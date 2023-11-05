32568cd073be676897e5d3cf88297188e999d115
Riki Pajri <109595793+Rikpaj@users.noreply.github.com>
2023-11-06 05:11:42 +0700

    update react js + vite
    1.import React from 'react' : gak ke baca.
    2.className='px-4 py-2 rounded-md bg-slate-400 text-white' : style taildwind gak ke baca
    3.component gak tampil di layar app

---
+++ Client/Wisata/src/components/Button.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const Button = () => {
+    return (
+        <div className='px-4 py-2 rounded-md bg-slate-400 text-white'>
+            Login
+        </div>
+    )
+}
+
+export default Button
---
+++ Client/Wisata/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className='w-sreen flex flex-row justify-center bg-black py-3 text-white'>
+            <p>Home</p>
+            <p>Login</p>
+            <p>Dasbord</p>
+            <p>About</p>
+        </div>
+    )
+}
+
+export default Navbar
