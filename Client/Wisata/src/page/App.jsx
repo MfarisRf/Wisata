42f5af0103b8cbd99afd22f034f4067c32f6c985
Riki Pajri <109595793+Rikpaj@users.noreply.github.com>
2023-11-09 21:34:37 +0700

    Update App.jsx

---
+++ Client/Wisata/src/page/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Button from '../components/Button'
+export const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Button />
+    </>
+  )
+}
+
+export default App
