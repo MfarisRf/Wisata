592035cf90f16dad6d74fc940cd48815e6138e1d
arya surya pratama <145951543+AryaSuryaPratama@users.noreply.github.com>
2023-11-10 21:51:19 +0700

    CardLoginForm

--- Client/Wisata/src/page/Login.jsx
+++ Client/Wisata/src/page/Login.jsx
@@ -1,12 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import CardLoginForm from '../components/CardLoginForm'
 import Navbar from '../components/Navbar'
 
 const Login = () => {
   return (
     <>
     <Navbar/>
-    but
+    <CardLoginForm />
     <div>login</div>
 
     </>
