0f54094617d9da34baf417c49548b55d8861fdb9
MUHAMAD FARIS RAFI <89460895+MfarisRf@users.noreply.github.com>
2023-11-10 20:21:36 +0700

    0.1.6

--- Client/Wisata/src/page/About.jsx
+++ Client/Wisata/src/page/About.jsx
@@ -1,12 +1,15 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
 
 const About = () => {
   return (
     <>
     <Navbar/>
     <div>about</div>
+    <Footer/>
     </>
     
   )
--- Client/Wisata/src/page/Dashboard.jsx
+++ Client/Wisata/src/page/Dashboard.jsx
@@ -1,9 +1,16 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 const Dashboard = () => {
   return (
+    <>
+    <Navbar/>
     <div>dasboard</div>
+    <Footer/>
+    </>
+
   )
 }
 
