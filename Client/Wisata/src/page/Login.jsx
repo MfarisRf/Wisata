f355bb1e62a520ef20e31354c1aba3e26a73e68e
MUHAMAD FARIS RAFI <89460895+MfarisRf@users.noreply.github.com>
2023-11-11 17:43:34 +0700

    1

--- Client/Wisata/src/page/Login.jsx
+++ Client/Wisata/src/page/Login.jsx
@@ -1,13 +1,11 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
-import CardLoginForm from '../components/CardLoginForm'
 import Navbar from '../components/Navbar'
 
 const Login = () => {
   return (
     <>
     <Navbar/>
-    <CardLoginForm/>
     <div>login</div>
 
     </>
