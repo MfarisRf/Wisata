01f62a1fff6ebc6fd72207ca816ce20791b5c566
Riki Pajri <109595793+Rikpaj@users.noreply.github.com>
2023-11-14 21:03:54 +0700

    update

--- Client/Wisata/src/components/Button_Profile.jsx
+++ Client/Wisata/src/components/Button_Profile.jsx
@@ -1,10 +1,11 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import icon from '../assets/icon/Icon_Profile.png'
 
 const Button_Profile = () => {
   return (
     <div>
-        <button type="button" className="py-2.5 px-5 me-2 mb-2 text-x15 font-bold text-green-700 focus:outline-none bg-f1f1e8 rounded-full border border-green-700 hover:bg-f1f1e8-100 hover:text-green-700 focus:z-10 focus:ring-2 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"><img src=""/>Masuk</button>
+        <button type="button" className="py-2.5 px-5 me-2 mb-2 text-x15 font-bold text-green-700 focus:outline-none bg-f1f1e8 rounded-full border border-green-700 hover:bg-f1f1e8-100 hover:text-green-700 focus:z-10 focus:ring-2 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"><img src={icon} className="img-fluid logo" alt="brand"/>Masuk</button>
     </div>
   )
 }
