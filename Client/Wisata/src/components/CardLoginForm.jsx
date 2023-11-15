52575ccd909c3d0143a30aca80ae39c370ce0269
arya surya pratama <145951543+AryaSuryaPratama@users.noreply.github.com>
2023-11-15 22:38:46 +0700

    1

--- Client/Wisata/src/components/CardLoginForm.jsx
+++ Client/Wisata/src/components/CardLoginForm.jsx
@@ -1,49 +1,65 @@
-// eslint-disable-next-line no-unused-vars
+// src/App.js
 import React from 'react';
 
-const CardLoginForm = () => {
+const App = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Implement login logic here
+    console.log('Login logic goes here');
+  };
+
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-f1f1e8 p-8 rounded shadow-md w-96">
         <h2 className="text-2xl font-semibold mb-4">Login</h2>
-        <form>
-          <div className="mb-4">
-            <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">
-              Email*
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className="w-full p-2 border rounded focus:outline-none focus:border-blue-500"
-              placeholder="masukan email"
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">
-              Kata sandi*
-            </label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              className="w-full p-2 border rounded focus:outline-none focus:border-blue-500"
-              placeholder="masukan kata sandi"
-            />
-          </div>
-          <div className="mb-4 flex justify-between items-center">
-            <a href="#" className="text-blue-500 text-sm">Lupa Kata Sandi?</a>
+    <div className="flex h-screen items-center justify-center">
+      <div className="bg-bfdcae w-96 shadow-md rounded-md overflow-hidden flex">
+        {/* Form Login */}
+        <div className="w-1/2 p-8">
+          <h2 className="text-2xl font-semibold mb-4">Masuk</h2>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-4">
+              <label htmlFor="username" className="block text-gray-700 text-sm font-bold mb-2">
+                Username
+              </label>
+              <input
+                type="text"
+                id="username"
+                name="username"
+                className="border rounded w-full py-1 px-3"
+                required
+              />
+            </div>
+            <div className="mb-6">
+              <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                className="border rounded w-full py-1 px-3"
+                required
+              />
+            </div>
             <button
               type="submit"
-              className="bg-blue-500 text-white p-2 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue"
+              className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue active:bg-blue-800"
             >
-              Masuk
+              Log In
             </button>
-          </div>
-        </form>
+          </form>
+        </div>
+
+        {/* Gambar Illustrasi */}
+        <div className="w-1/2 bg-gray-400 flex items-center justify-center">
+          <img
+            src="" // Ganti dengan URL gambar yang diinginkan
+            alt="Illustration"
+            className="object-cover w-full h-full rounded-r-md"
+          />
+        </div>
       </div>
     </div>
   );
 };
 
-export default CardLoginForm;
+export default App;
