d2db21e852ba6025bfdb6a7bcc1d8c7fa5d89b24
Riki Pajri <109595793+Rikpaj@users.noreply.github.com>
2023-11-17 01:40:04 +0700

    update

--- Client/Wisata/src/page/Landingpage.jsx
+++ Client/Wisata/src/page/Landingpage.jsx
@@ -3,34 +3,37 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import Gambar from '../assets/images/Cirebon.png'
+import background from "../assets/images/Tekstur.png";
 
 const Landingpage = () => {
   return (
     <div>
         <>
         <Navbar />
-        <div id="animation-carousel" className="relative w-full" data-carousel="static">
+        {/* carosel */}
+
+<div id="default-carousel" className="relative w-full" data-carousel="slide">
     {/* <!-- Carousel wrapper --> */}
     <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
          {/* <!-- Item 1 --> */}
-        <div className="hidden duration-200 ease-linear" data-carousel-item>
-            <img src={Gambar} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="Cirebon"/>
+        <div className="hidden duration-700 ease-in-out" data-carousel-item>
+            <img src={Gambar} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
         </div>
         {/* <!-- Item 2 --> */}
-        <div className="hidden duration-200 ease-linear" data-carousel-item>
-            <img src="/docs/images/carousel/carousel-2.svg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+        <div className="hidden duration-700 ease-in-out" data-carousel-item>
+            <img src={Gambar} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
         </div>
         {/* <!-- Item 3 --> */}
-        <div className="hidden duration-200 ease-linear" data-carousel-item="active">
-            <img src="/docs/images/carousel/carousel-3.svg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+        <div className="hidden duration-700 ease-in-out" data-carousel-item>
+            <img src="/docs/images/carousel/carousel-3.svg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
         </div>
         {/* <!-- Item 4 --> */}
-        <div className="hidden duration-200 ease-linear" data-carousel-item>
-            <img src="/docs/images/carousel/carousel-4.svg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+        <div className="hidden duration-700 ease-in-out" data-carousel-item>
+            <img src="/docs/images/carousel/carousel-4.svg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
         </div>
         {/* <!-- Item 5 --> */}
-        <div className="hidden duration-200 ease-linear" data-carousel-item>
-            <img src="/docs/images/carousel/carousel-5.svg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+        <div className="hidden duration-700 ease-in-out" data-carousel-item>
+            <img src="/docs/images/carousel/carousel-5.svg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
         </div>
     </div>
     {/* <!-- Slider controls --> */}
@@ -50,6 +53,43 @@ const Landingpage = () => {
             <span className="sr-only">Next</span>
         </span>
     </button>
+</div>
+
+
+{/* Keterangan */}
+<div className='bg-f1f1e8' style={{ backgroundImage: `url(${background})` }}>
+    <h3 className='text-center font-bold pb-10'>Tentang CIAYUMAJAKUNING</h3>
+        <p className='text-center font-sm px-96'>Ciayumajakuning adalah sebuah wilayah di Jawa Barat, Indonesia yang terdiri dari Kota Cirebon, Kabupaten Cirebon, Kabupaten Indramayu, Kabupaten Majalengka, dan Kabupaten Kuningan. Wilayah ini merupakan kekuatan ekonomi yang baru dan besar di Jawa Barat sesudah kawasan Bandung Raya.</p>
+    <div className='w-4/6 ml-60 mt-20 p-5 border-4 border-green-500 rounded-lg bg-f1f1e8'>
+        <h2>Recomendasi Wisata</h2>
+        {/* image */}
+    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <a href="#">
+        <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="wisata cirebon" />
+    </a>
+    <a href="#">
+        <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="wisata cirebon" />
+    </a>
+    <a href="#">
+        <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="wisata cirebon" />
+    </a>
+    <a href="#">
+        <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="wisata cirebon" />
+    </a>
+    <div className="p-5">
+        <a href="#">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Wisata cirebon</h5>
+        </a>
+        <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        Lihat selengkapnya
+        <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+        </svg>
+        </a>
+    </div>
+</div>
+
+{/* tanda */}
+    </div>
 </div>
         <Footer />
         </>
