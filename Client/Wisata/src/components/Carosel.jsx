80a874b877d01033c75c283579d5580400838e00
Riki Pajri <109595793+Rikpaj@users.noreply.github.com>
2023-11-17 23:57:16 +0700

    update

---
+++ Client/Wisata/src/components/Carosel.jsx
@@ -0,0 +1,53 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import Gambar1 from '../assets/images/Cirebon.png'
+import Gambar2 from '../assets/images/indramayu.png'
+import Gambar3 from '../assets/images/majalengka.png'
+import Gambar4 from '../assets/images/Kuningan.png'
+
+const Carosel = () => {
+  return (
+    <div>
+        <div id="default-carousel" className="relative w-full pt-90" data-carousel="slide">
+    {/* <!-- Carousel wrapper --> */}
+    <div className="relative h-56 overflow-hidden md:h-96">
+         {/* <!-- Item 1 --> */}
+        <div className="hidden duration-700 ease-in-out" data-carousel-item>
+            <img src={Gambar1} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
+        </div>
+        {/* <!-- Item 2 --> */}
+        <div className="hidden duration-700 ease-in-out" data-carousel-item>
+            <img src={Gambar2} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
+        </div>
+        {/* <!-- Item 3 --> */}
+        <div className="hidden duration-700 ease-in-out" data-carousel-item>
+            <img src={Gambar3} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
+        </div>
+        {/* <!-- Item 4 --> */}
+        <div className="hidden duration-700 ease-in-out" data-carousel-item>
+            <img src={Gambar4} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
+        </div>
+    </div>
+    {/* <!-- Slider controls --> */}
+    <button type="button" className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 1 1 5l4 4"/>
+            </svg>
+            <span className="sr-only">Previous</span>
+        </span>
+    </button>
+    <button type="button" className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4"/>
+            </svg>
+            <span className="sr-only">Next</span>
+        </span>
+    </button>
+</div>
+    </div>
+  )
+}
+
+export default Carosel
