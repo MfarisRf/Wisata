c432d3233f82a0875c66649f704e5f208a99125a
MUHAMAD FARIS RAFI <89460895+MfarisRf@users.noreply.github.com>
2023-11-18 01:45:36 +0700

    DB+Pronend

--- Server/config/Database.js
+++ Server/config/Database.js
@@ -1,7 +1,5 @@
 import {Sequelize} from "sequelize";
 
-cont Sequelize = require('sequelize');
-
 const db = new Sequelize('db_wisata', 'root', '', {
     host: 'localhost',
     dialect: 'mysql'
--- Server/server.js
+++ Server/server.js
@@ -1,16 +1,18 @@
-import express from "express";
+import express, { Router } from "express";
 import cors from "cors";
-import db from "./config/Database";
+import db from "./config/Database.js";
+import router from "./routes/index.js";
+import Admin from "./models/AdminModel.js";
+
+const app = express();
 
 try {
     await db.authenticate();
-    console.log('Connection has been established successfully.');
+    console.log('DB Connected...');
 } catch (error) {
     console.error('Unable to connect to the database:',error);
 }
-
-const app = express();
-app.use(cors())
-app.use(express.json());
+app.use((express.json()));
+app.use(Router);
 
 app.listen(5000, ()=> console.log('Server up and running...'));
