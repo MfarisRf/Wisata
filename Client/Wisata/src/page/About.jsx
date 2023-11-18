5664d4f1a787b22c833c4c4ad28b33406648ae91
arya surya pratama <145951543+AryaSuryaPratama@users.noreply.github.com>
2023-11-18 15:11:09 +0700

    update

--- Client/Wisata/src/page/About.jsx
+++ Client/Wisata/src/page/About.jsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
-import Navbar from '../Component/Navbar'
+import Navbar from '../component/Navbar'
 
 function About () {
     return (
