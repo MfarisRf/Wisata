784c4cf1eb4b4f043409a7244684c53ac088e055
MUHAMAD FARIS RAFI <89460895+MfarisRf@users.noreply.github.com>
2023-11-18 10:42:09 +0700

    Update Server

--- Server/server.js
+++ Server/server.js
@@ -1,8 +1,9 @@
-import express, { Router } from "express";
+import express from "express";
 import cors from "cors";
 import db from "./config/Database.js";
 import router from "./routes/index.js";
 import Admin from "./models/AdminModel.js";
+import dotenv from "dotenv";
 
 const app = express();
 
@@ -12,7 +13,6 @@ try {
 } catch (error) {
     console.error('Unable to connect to the database:',error);
 }
-app.use((express.json()));
-app.use(Router);
-
+app.use(express.json());
+app.use(router)
 app.listen(5000, ()=> console.log('Server up and running...'));
