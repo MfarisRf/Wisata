ca5789b3f0405bb1d91fe401c9ef3def56e0761c
Muhammad Ghandi Azhari <145952262+ghandiazhari29@users.noreply.github.com>
2023-11-20 13:46:05 +0700

    nyoba 1

--- Client/Wisata/src/page/About.jsx
+++ Client/Wisata/src/page/About.jsx
@@ -1,12 +1,10 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
-import Navbar from '../Component/Navbar'
 
 function About () {
     return (
        <div>
         <>
-        <Navbar/>
         <div className='bg-f1f1e8' style={{ backgroundImage: `url(${background})`}}>
             <h3 className='text-center text-2xl font-Boogaloo font-bold text-456761 pb-10 pt-28'>About</h3>
                 <p className='text-center font-sm px-25 text-456761'>Selamat datang di Ciayumajakuning! Kami adalah tempat di mana petualangan dimulai dan kenangan tak terlupakan dibuat. Sejak diluncurkan pada tahun 2023, kami telah berkomitmen untuk menjadi teman setia perjalanan Anda, membantu Anda menjelajahi destinasi indah yang ada di CIAYUMAJAKUNING.</p>
