536ed13d5f8f9737e22281ac5ea897a2c9f1a144
Muhammad Ghandi Azhari <145952262+ghandiazhari29@users.noreply.github.com>
2023-11-20 14:22:16 +0700

    about

--- Client/Wisata/src/page/About.jsx
+++ Client/Wisata/src/page/About.jsx
@@ -1,20 +1,19 @@
-// eslint-disable-next-line no-unused-vars
 import React from 'react'
-<<<<<<< HEAD
-=======
 import Navbar from '../components/Navbar'
->>>>>>> 561e056b7ea81e0ef53808049742488c0031ba9a
 
-function About () {
-    return (
-       <div>
+const About = () => {
+  return (
+    <div>
         <>
-        <div className='bg-f1f1e8' style={{ backgroundImage: `url(${background})`}}>
-            <h3 className='text-center text-2xl font-Boogaloo font-bold text-456761 pb-10 pt-28'>About</h3>
-                <p className='text-center font-sm px-25 text-456761'>Selamat datang di Ciayumajakuning! Kami adalah tempat di mana petualangan dimulai dan kenangan tak terlupakan dibuat. Sejak diluncurkan pada tahun 2023, kami telah berkomitmen untuk menjadi teman setia perjalanan Anda, membantu Anda menjelajahi destinasi indah yang ada di CIAYUMAJAKUNING.</p>
+        <Navbar />
+          <img className> </img>
+        |<div> 
+            <h3 className='text-center text-2xl font-Boogaloo text-456761 pb-10 pt-28 font-bold'>Tentang Kami</h3>
+            <p className='text-center font-sm px-25 text-456761'>Selamat datang di Ciayumajakuning! Kami adalah tempat di mana petualangan dimulai dan kenangan tak terlupakan dibuat. Sejak diluncurkan pada tahun 2023, kami telah berkomitmen untuk menjadi teman setia perjalanan Anda, membantu Anda menjelajahi destinasi indah yang ada di CIAYUMAJAKUNING. </p>
         </div>
         </>
-       </div>
-    )
+    </div>
+  )
 }
+
 export default About
