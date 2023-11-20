2f37060e4b17fbf75227ec0c961b7f7da2adf447
arya surya pratama <145951543+AryaSuryaPratama@users.noreply.github.com>
2023-11-21 01:12:50 +0700

    baru segitu mau ke warkop dulu ngopi

--- Client/Wisata/src/App.jsx
+++ Client/Wisata/src/App.jsx
@@ -3,6 +3,9 @@ import Home from './page/Home'
 import Login from './page/login'
 import About from './page/About'
 import Landingpage from './page/Landingpage'
+import Destinasiwisata from './page/Destinasiwisata'
+import Kuliner from './page/Kuliner'
+import Oleholeh from './page/Oleholeh'
 import OurTeam from './components/OurTeam'
 import Notfoundkuliner from './page/Notfoundkuliner'
 import Notfoundwisata from './page/Notfoundwisata'
@@ -15,6 +18,9 @@ const App = () => {
       <Router>
         <Routes>
           <Route path="/" element={<Landingpage />} />
+          <Route path="/Destinasiwisata" element={<Destinasiwisata/>} /> 
+          <Route path="/Kuliner" element={<Kuliner/>} />
+          <Route path="/Oleholeh" element={<Oleholeh/>} />
           <Route path="/Login" element={<Login />} />
           <Route path="/About" element={<About />} />
           <Route path="/Notfoundkuliner" element={<Notfoundkuliner/>} />
--- Client/Wisata/src/page/Notfoundkuliner.jsx
+++ Client/Wisata/src/page/Notfoundkuliner.jsx
@@ -13,15 +13,16 @@ function Notfoundkuliner() {
     <br/>
     <div className="flex items-center justify-between w-full sm:w-96">
 
-    <div className="relative w-full mr-2">
-        <input type="search" id="search-dropdown" className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" placeholder="Search" required />
-        <button type="submit" className="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-            </svg>
-            <span className="sr-only">Search</span>
-        </button>
-    </div>
+    <div class="relative w-full mr-2">
+  <input type="search" id="search-dropdown" class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" placeholder="Search" required />
+
+  <button type="submit" class="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-gray-900 bg-white bg-opacity-80 rounded-e-lg border border-gray-300 hover:bg-opacity-90 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-gray-800 dark:border-s-gray-700 dark:border-gray-700 dark:text-white dark:focus:border-blue-500 dark:hover:bg-gray-900 dark:focus:ring-blue-800">
+    <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+    </svg>
+    <span class="sr-only">Search</span>
+  </button>
+</div>
 
     <form>
         <div className="relative flex-shrink-0 z-10 inline-flex items-center">
@@ -32,16 +33,16 @@ function Notfoundkuliner() {
             <div id="dropdown" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
                 <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdown-button">
                     <li>
-                        <button type="button" className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">HALLO</button>
+                        <button type="button" className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Cirebon</button>
                     </li>
                     <li>
-                        <button type="button" className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">GUA</button>
+                        <button type="button" className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Indramayu</button>
                     </li>
                     <li>
-                        <button type="button" className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">GUA</button>
+                        <button type="button" className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Majalengka</button>
                     </li>
                     <li>
-                        <button type="button" className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">SIAPA</button>
+                        <button type="button" className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Kuningan</button>
                     </li>
                 </ul>
             </div>
