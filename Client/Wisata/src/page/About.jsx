4ffa669af0ba3b1ae23a1ecea5f65ec20abfd138
Muhammad Ghandi Azhari <145952262+ghandiazhari29@users.noreply.github.com>
2023-11-22 01:55:39 +0700

    about belum ok

--- Client/Wisata/src/page/About.jsx
+++ Client/Wisata/src/page/About.jsx
@@ -9,17 +9,19 @@ const About = () => {
     <div>
         <>
         <Navbar />
-          <img className=""> </img>
+
         |<div> 
-            <h3 className='text-center text-2xl font-Libre Baskerville text-456761 pb-10 pt-28 font-bold'>Tentang Kami</h3>
-            <p className='text-center font-Lato px-25 text-456761'>Selamat datang di Ciayumajakuning! Kami adalah tempat di mana petualangan dimulai dan kenangan tak terlupakan dibuat. Sejak diluncurkan pada tahun 2023, kami telah berkomitmen untuk menjadi teman setia perjalanan Anda, membantu Anda menjelajahi destinasi indah yang ada di CIAYUMAJAKUNING. </p>
+            <h3 className='text-justify text-align:-left text-2xl font-Libre Baskerville text-456761 pb-10 pt-28 text-45992E font-bold'>Tentang Kami</h3>
+            <p className='text-center font-Lato px-25 text-align:-left text-45992E'>Selamat datang di Ciayumajakuning! Kami adalah tempat di mana petualangan dimulai dan kenangan tak terlupakan dibuat. Sejak diluncurkan pada tahun 2023, kami telah berkomitmen untuk menjadi teman setia perjalanan Anda, membantu Anda menjelajahi destinasi indah yang ada di CIAYUMAJAKUNING. </p>
             <a href="#">
-                <img className="w-234" src={Logo} alt="Explore Ciayumajakuning"/>
+                <img className="w-760" padding- src={Logo} alt="Explore Ciayumajakuning"/>
         <div>
-            <h3 className='text-center text-2x1 font-Libre Baskerville text-456761 pb-10 pt-28 font-bold'>EXPLORE CIAYUMAJAKUUNING</h3>
+            <h3 className='text-center text-2x1 font-Libre Baskerville text-456761 pb-10 pt-28 font-bold'>EXPLORE CIAYUMAJAKUNING</h3>
             <p className='text-center font-Lato px-25 text-456761'>Explore Ciayumajakuning adalah platform online yang berguna untuk sarana promosi bagi para pelaku usaha pariwisata. tidak hanya itu, Explore Ciayumajakuning hadir sebagai panduan Anda sebelum memutuskan untuk berkunjung ke sebuah wisata, khususnya yang ada di daerah ciayumajakuning. Memastikan kemudahan Anda dalam merencanakan atau menyiapkan perjalanan berpetualang adalah tujuan kami.</p>
             <a href="=">
                 <img className="w-234" src={foto} alt="logo nama bawah"/>
+            <h3 className='text-center text-2x1 font-Libre Baskerville text-456761 pb-10 pt-28 font-bold'>MISI</h3>
+            <p className='text-center font-Lato px-25 text-456761'>Memberikan pengalaman perjalanan yang tak terlupakan. Kami percaya bahwa setiap perjalanan adalah kesempatan untuk belajar, tumbuh, dan menghargai keindahan keberagaman dunia. Kami berkomitmen untuk memberikan layanan terbaik, mempromosikan keberlanjutan, dan mendukung komunitas lokal di destinasi wisata.</p>
             </a>
         </div>
             </a> 
