a197a29796138f70a39bbfe2c8d9753d7c6e7b9e
MUHAMAD FARIS RAFI <89460895+MfarisRf@users.noreply.github.com>
2023-11-23 01:24:56 +0700

    Update About

--- Client/Wisata/src/components/OurTeam.jsx
+++ Client/Wisata/src/components/OurTeam.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import FotoTeamFaris from '../assets/images/FotoTeamFaris.png'
 const OurTeam = () => {
   return (
         <section class="text-gray-600 body-font">
@@ -20,7 +20,7 @@ const OurTeam = () => {
         </div>
         <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
             <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/84x84"/>
+                <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={FotoTeamFaris}/>
             <div class="flex-grow">
                 <h2 class="text-gray-900 title-font font-medium">Muhamad Faris Rafi</h2>
                 <p class="text-gray-500">Project Manager</p>
