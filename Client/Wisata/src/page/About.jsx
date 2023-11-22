04eda29f60217105f3a7a821b0a71f5e6f321baa
Muhammad Ghandi Azhari <145952262+ghandiazhari29@users.noreply.github.com>
2023-11-22 21:58:14 +0700

    lupa

--- Client/Wisata/src/page/About.jsx
+++ Client/Wisata/src/page/About.jsx
@@ -15,6 +15,7 @@ const About = () => {
             <p className='text-center font-Lato px-25 text-align:-left text-45992E'>Selamat datang di Ciayumajakuning! Kami adalah tempat di mana petualangan dimulai dan kenangan tak terlupakan dibuat. Sejak diluncurkan pada tahun 2023, kami telah berkomitmen untuk menjadi teman setia perjalanan Anda, membantu Anda menjelajahi destinasi indah yang ada di CIAYUMAJAKUNING. </p>
             <a href="#">
                 <img className="w-760" padding- src={Logo} alt="Explore Ciayumajakuning"/>
+                
         <div>
             <h3 className='text-center text-2x1 font-Libre Baskerville text-456761 pb-10 pt-28 font-bold'>EXPLORE CIAYUMAJAKUNING</h3>
             <p className='text-center font-Lato px-25 text-456761'>Explore Ciayumajakuning adalah platform online yang berguna untuk sarana promosi bagi para pelaku usaha pariwisata. tidak hanya itu, Explore Ciayumajakuning hadir sebagai panduan Anda sebelum memutuskan untuk berkunjung ke sebuah wisata, khususnya yang ada di daerah ciayumajakuning. Memastikan kemudahan Anda dalam merencanakan atau menyiapkan perjalanan berpetualang adalah tujuan kami.</p>
