5d180c2da3bc2b8895f6dafc2040bda82225b2ab
Riki Pajri <109595793+Rikpaj@users.noreply.github.com>
2023-11-22 23:27:16 +0700

    update

---
+++ Client/Wisata/src/components/Layer_lp.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const Layer_lp = () => {
+  return (
+    <div className='bg-[#1E9469] px-80'>
+
+    </div>
+  )
+}
+
+export default Layer_lp
