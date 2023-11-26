e8a7cf2e30ccd2ccc8b69f1f46cbc297084222b2
Muhammad Ghandi Azhari <145952262+ghandiazhari29@users.noreply.github.com>
2023-11-26 20:49:10 +0700

    masih about dan gabisa juga... capeee

--- Client/Wisata/src/page/About.jsx
+++ Client/Wisata/src/page/About.jsx
@@ -16,10 +16,15 @@ const About = () => {
             <br/>
             <br/>
             <br/>
-            <h3 className='text-right text-2x1 text-Libre Baskerville sm:text-3xl text-2xl font-bold title-font mb-4 text-gray-900'>MISI</h3>
-            <p className='text-justify text-left px-40 font-Lato px-10 text-29446F'>Memberikan pengalaman perjalanan yang tak terlupakan. Kami percaya bahwa setiap perjalanan adalah kesempatan untuk belajar, tumbuh, dan menghargai keindahan keberagaman dunia. Kami berkomitmen untuk memberikan layanan terbaik, mempromosikan keberlanjutan, dan mendukung komunitas lokal di destinasi wisata.</p>
-            <OurTeam/>
-            <Footer/>
+            <div className="flex items-center gap-6 justify-center">
+                <div className="w1/2">
+                  <h3 className='text-right text-2x1 text-Libre Baskerville sm:text-3xl text-2xl font-bold title-font mb-4 text-gray-900'>MISI</h3>
+                  <p className='text-justify text-left px-10 font-Lato px-20 text-29446F'>Memberikan pengalaman perjalanan yang tak terlupakan. Kami percaya bahwa setiap perjalanan adalah kesempatan untuk belajar, tumbuh, dan menghargai keindahan keberagaman dunia. 
+                  Kami berkomitmen untuk memberikan layanan terbaik, mempromosikan keberlanjutan, dan mendukung komunitas lokal di destinasi wisata.</p>
+                  <OurTeam/>
+                  <Footer/>
+                </div>
+            </div>
         </div>
     </div>
   )
