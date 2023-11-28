f9f91d7aa0d380ac514b227be0335033ef4c5c5a
Riki Pajri <109595793+Rikpaj@users.noreply.github.com>
2023-11-28 23:45:56 +0700

    update about

--- Client/Wisata/src/page/About.jsx
+++ Client/Wisata/src/page/About.jsx
@@ -5,10 +5,12 @@ import AboutKami from '../components/aboutkami'
 import AboutCiayu from '../components/AboutCiayu'
 import OurTeam from '../components/OurTeam'
 import Footer from '../components/Footer'
+import background2 from "../assets/images/Teksturs3.png";
+import background0 from "../assets/images/Backgroud_about.png";
 
 const About = () => {
   return (
-    <div>  
+    <div className='bg-cover bg-[#F1F1E8]' >
         <Navbar/>
         <div>
             <AboutKami/>
@@ -17,10 +19,10 @@ const About = () => {
             <br/>
             <br/>
             <div className="flex items-center gap-6 justify-center">
-                <div className="w1/2">
-                  <h3 className='text-right text-2x1 text-Libre Baskerville sm:text-3xl text-2xl font-bold title-font mb-4 text-gray-900'>MISI</h3>
-                  <p className='text-justify text-left px-10 font-Lato px-20 text-29446F'>Memberikan pengalaman perjalanan yang tak terlupakan. Kami percaya bahwa setiap perjalanan adalah kesempatan untuk belajar, tumbuh, dan menghargai keindahan keberagaman dunia. 
-                  Kami berkomitmen untuk memberikan layanan terbaik, mempromosikan keberlanjutan, dan mendukung komunitas lokal di destinasi wisata.</p>
+                <div className="w1/" style={{backgroundImage: `url(${background2}) `}}>
+                  <p className="font-Lato text-29446F px-96 pl-56 mr-20 w-full h-72 p-14 font-['Lato'] " style={{backgroundImage: `url(${background0}) `}}>
+                  <h3 className=" flex card text-2x1 text-Libre Baskerville sm:text-3xl text-2xl font-bold title-font mb-4 text-gray-900 font-['Baskerville']">MISI</h3>
+                    Memberikan pengalaman perjalanan yang tak terlupakan. Kami percaya bahwa setiap perjalanan adalah kesempatan untuk belajar, tumbuh, dan menghargai keindahan keberagaman dunia.Kami berkomitmen untuk memberikan layanan terbaik, mempromosikan keberlanjutan, dan mendukung komunitas lokal di destinasi wisata.</p>
                   <OurTeam/>
                   <Footer/>
                 </div>
