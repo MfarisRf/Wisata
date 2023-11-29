35b1a35d486d35fd017a94f5fdc9ae890aada4f6
Muhammad Ghandi Azhari <145952262+ghandiazhari29@users.noreply.github.com>
2023-11-29 23:37:05 +0700

    detailwisata

---
+++ Client/Wisata/src/page/DetailWisata.jsx
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+import 'flowbite'
+
+// import background from "../assets/images/Teksturs.png";
+import GambarDetwisCrb from '../assets/images/DetwisCrb.png'
