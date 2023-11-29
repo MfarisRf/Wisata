0432c5d979621320c305bfc73e6251854928a9b0
Riki Pajri <109595793+Rikpaj@users.noreply.github.com>
2023-11-29 10:32:28 +0700

    update halaman about
    catatan :
    1. 'Misi' posisi gak sesuai
    2. di ourteam gak sesuai

--- Client/Wisata/src/components/OurTeam.jsx
+++ Client/Wisata/src/components/OurTeam.jsx
@@ -8,79 +8,76 @@ import Riki from '../assets/images/Foto TeamRiki.png'
 import Ghandi from '../assets/images/Foto TeamGhandi.png'
 import Arya from '../assets/images/Foto TeamArya.png'
 
-
-import FotoTeamFaris from '../assets/images/FotoTeamFaris.png'
-
 const OurTeam = () => {
   return (
         <section className="text-gray-600 body-font">
     <div className="container px-5 py-24 mx-auto ">
         <div className="flex flex-col text-center w-full mb-20">
-        <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Our Team</h1>
-        <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Mari bersama-sama menjelajahi, belajar, dan menciptakan kenangan seumur hidup. Selamat datang di tim kami, tempat di mana impian perjalanan Anda menjadi kenyataan.</p>
+        <h1 className="sm:text-3xl font-['Heeboo'] text-[#FFFFFF] text-2xl font-bold title-font mb-4">Our Team</h1>
+        <p className="lg:w-2/3 mx-auto leading-relaxed text-base text-[#FFFFFF]">Mari bersama-sama menjelajahi, belajar, dan menciptakan kenangan seumur hidup. Selamat datang di tim kami, tempat di mana impian perjalanan Anda menjadi kenyataan.</p>
         </div>
         <div className="flex flex-wrap -m-2">
         <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-            <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+            <div className="h-full flex items-center border-[#0A5539] border-2 p-4 rounded-lg">
             <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={Vani}/>
             <div className="flex-grow">
-                <h2 className="text-gray-900 title-font font-medium">Vani Julia Wulandari</h2>
-                <p className="text-gray-500">UI Designer</p>
+                <h2 className="text-[#FFFFFF]  title-font font-medium">Vani Julia Wulandari</h2>
+                <p className="text-[#FFFFFF] ">UI Designer</p>
             </div>
             </div>
         </div>
         <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-            <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={FotoTeamFaris}/>
+            <div className="h-full flex items-center border-[#0A5539] border-2 p-4 rounded-lg">
+                <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={Faris}/>
 
             <div className="flex-grow">
-                <h2 className="text-gray-900 title-font font-medium">Muhamad Faris Rafi</h2>
-                <p className="text-gray-500">Project Manager</p>
+                <h2 className="text-[#FFFFFF]  title-font font-medium">Muhamad Faris Rafi</h2>
+                <p className="text-[#FFFFFF] ">Project Manager</p>
             </div>
             </div>
         </div>
         <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-            <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+            <div className="h-full flex items-center border-[#0A5539] border-2 p-4 rounded-lg">
             <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={Niken} />
             <div className="flex-grow">
-                <h2 className="text-gray-900 title-font font-medium">Niken Dwi Setianingsih</h2>
-                <p className="text-gray-500">UI Designer</p>
+                <h2 className="text-[#FFFFFF]  title-font font-medium">Niken Dwi Setianingsih</h2>
+                <p className="text-[#FFFFFF] ">UI Designer</p>
             </div>
             </div>
         </div>
         <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-            <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+            <div className="h-full flex items-center border-[#0A5539] border-2 p-4 rounded-lg">
             <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={Andre} />
             <div className="flex-grow">
-                <h2 className="text-gray-900 title-font font-medium">Andrean Maulana Artiatno</h2>
-                <p className="text-gray-500">UI Designer</p>
+                <h2 className="text-[#FFFFFF]  title-font font-medium">Andrean Maulana Artiatno</h2>
+                <p className="text-[#FFFFFF] ">UI Designer</p>
             </div>
             </div>
         </div>
         <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-            <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+            <div className="h-full flex items-center border-[#0A5539] border-2 p-4 rounded-lg">
             <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={Ghandi} />
             <div className="flex-grow">
-                <h2 className="text-gray-900 title-font font-medium">Muhammad Ghandi Azhari</h2>
-                <p className="text-gray-500">Programmer</p>
+                <h2 className="text-[#FFFFFF]  title-font font-medium">Muhammad Ghandi Azhari</h2>
+                <p className="text-[#FFFFFF] ">Programmer</p>
             </div>
             </div>
         </div>
         <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-            <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+            <div className="h-full flex items-center border-[#0A5539] border-2 p-4 rounded-lg">
             <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={Riki} />
             <div className="flex-grow">
-                <h2 className="text-gray-900 title-font font-medium">Riki Pajri</h2>
-                <p className="text-gray-500">Programmer</p>
+                <h2 className="text-[#FFFFFF] title-font font-medium">Riki Pajri</h2>
+                <p className="text-[#FFFFFF] ">Programmer</p>
             </div>
             </div>
         </div>
         <div className="p-2 lg:w-1/3 md:w-1/2 w-full align-middle">
-            <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+            <div className="h-full flex items-center border-[#0A5539] border-2 p-4 rounded-lg">
             <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={Arya} />
             <div className="flex-grow">
-                <h2 className="text-gray-900 title-font font-medium">Arya Surya Pratama</h2>
-                <p className="text-gray-500">Programmer</p>
+                <h2 className="text-[#FFFFFF] title-font font-medium">Arya Surya Pratama</h2>
+                <p className="text-[#FFFFFF]">Programmer</p>
             </div>
             </div>
         </div>
--- Client/Wisata/src/page/About.jsx
+++ Client/Wisata/src/page/About.jsx
@@ -20,8 +20,8 @@ const About = () => {
             <br/>
             <div className="flex items-center gap-6 justify-center">
                 <div className="w1/" style={{backgroundImage: `url(${background2}) `}}>
-                  <p className="font-Lato text-29446F px-96 pl-56 mr-20 w-full h-72 p-14 font-['Lato'] " style={{backgroundImage: `url(${background0}) `}}>
-                  <h3 className=" flex card text-2x1 text-Libre Baskerville sm:text-3xl text-2xl font-bold title-font mb-4 text-gray-900 font-['Baskerville']">MISI</h3>
+                  <p className="font-Lato text-justify text-29446F px-96 pl-56 mr-20 w-full h-72 p-14 font-['Lato'] " style={{backgroundImage: `url(${background0}) `}}>
+                  <h3 className=" flex card text-2x1 text-justify Baskerville sm:text-3xl text-2xl font-bold title-font mb-4 text-gray-900 font-['Baskerville']">MISI</h3>
                     Memberikan pengalaman perjalanan yang tak terlupakan. Kami percaya bahwa setiap perjalanan adalah kesempatan untuk belajar, tumbuh, dan menghargai keindahan keberagaman dunia.Kami berkomitmen untuk memberikan layanan terbaik, mempromosikan keberlanjutan, dan mendukung komunitas lokal di destinasi wisata.</p>
                   <OurTeam/>
                   <Footer/>
