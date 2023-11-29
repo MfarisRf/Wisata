ca298d44644951683ab6b19bcbfcb5a6b1d8c257
MUHAMAD FARIS RAFI <89460895+MfarisRf@users.noreply.github.com>
2023-11-29 21:48:09 +0700

    nih pemai

--- Client/Wisata/src/components/OurTeam.jsx
+++ Client/Wisata/src/components/OurTeam.jsx
@@ -26,7 +26,7 @@ const OurTeam = () => {
             </div>
             </div>
         </div>
-        <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
+        <div className="p-2 lg:w-1/3 md:w-1/2 w-full col-start-2 col-span-4">
             <div className="h-full flex items-center border-[#0A5539] border-2 p-4 rounded-lg">
                 <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={Faris}/>
 
--- Client/Wisata/src/page/Destinasiwisata.jsx
+++ Client/Wisata/src/page/Destinasiwisata.jsx
@@ -68,8 +68,113 @@ function Destinasiwisata() {
 </form>
 </div>
 <br/>
+<br/>
 <div>
-  <div className="grid grid-cols-1 gap-6   lg:grid-cols-4 ">
+  <div className="grid px-12 grid-cols-1 gap-6 lg:grid-cols-4 ">
+            <a href="#" className=" shadow-2xl relative ">
+                <div className="  h-full relative shadow-2xl shadow-green-900 overflow-hidden group ">
+                    <div className=" absolute -bottom-10 group-hover:top-0 left-0 w-full h-full group-hover:bg-green-900 transition-all ease-in-out duration-500  ">
+                        <div className="w-full h-full   p-5   relative">
+                            <div className="absolute bottom-0 group-hover:bottom-24 text-white  text-left   transition-all ease-in-out duration-500 ">
+                                <h2 className="mb-2 text-10 font-bold pl-3 pt-1">Pantai Kejawanan</h2>
+                                <p className="mb-2 text-10 font-bold pl-3 pt-1">Lihat Selengkapnya</p>
+                            </div>
+                        </div>
+                    </div>
+                    <img src={GambarPK} className="w-full z-0  h-full    object-fill example "></img>
+                </div>
+            </a>
+            <a href="#" className=" shadow-2xl relative ">
+                <div className="  h-full relative shadow-2xl shadow-green-900 overflow-hidden group ">
+                    <div className=" absolute -bottom-10 group-hover:top-0 left-0 w-full h-full group-hover:bg-green-900 transition-all ease-in-out duration-500  ">
+                        <div className="w-full h-full   p-5   relative">
+                            <div className="absolute bottom-0 group-hover:bottom-24 text-white  text-left   transition-all ease-in-out duration-500 ">
+                                <h2 className="mb-2 text-10 font-bold pl-3 pt-1">Pantai Kejawanan</h2>
+                                <p className="mb-2 text-10 font-bold pl-3 pt-1">Lihat Selengkapnya</p>
+                            </div>
+                        </div>
+                    </div>
+                    <img src={GambarPK} className="w-full z-0  h-full    object-fill example "></img>
+                </div>
+            </a>
+            <a href="#" className=" shadow-2xl relative ">
+                <div className="  h-full relative shadow-2xl shadow-green-900 overflow-hidden group ">
+                    <div className=" absolute -bottom-10 group-hover:top-0 left-0 w-full h-full group-hover:bg-green-900 transition-all ease-in-out duration-500  ">
+                        <div className="w-full h-full   p-5   relative">
+                            <div className="absolute bottom-0 group-hover:bottom-24 text-white  text-left   transition-all ease-in-out duration-500 ">
+                                <h2 className="mb-2 text-10 font-bold pl-3 pt-1">Pantai Kejawanan</h2>
+                                <p className="mb-2 text-10 font-bold pl-3 pt-1">Lihat Selengkapnya</p>
+                            </div>
+                        </div>
+                    </div>
+                    <img src={GambarPK} className="w-full z-0  h-full    object-fill example "></img>
+                </div>
+            </a>
+            <a href="#" className=" shadow-2xl relative ">
+                <div className="  h-full relative shadow-2xl shadow-green-900 overflow-hidden group ">
+                    <div className=" absolute -bottom-10 group-hover:top-0 left-0 w-full h-full group-hover:bg-green-900 transition-all ease-in-out duration-500  ">
+                        <div className="w-full h-full   p-5   relative">
+                            <div className="absolute bottom-0 group-hover:bottom-24 text-white  text-left   transition-all ease-in-out duration-500 ">
+                                <h2 className="mb-2 text-10 font-bold pl-3 pt-1">Pantai Kejawanan</h2>
+                                <p className="mb-2 text-10 font-bold pl-3 pt-1">Lihat Selengkapnya</p>
+                            </div>
+                        </div>
+                    </div>
+                    <img src={GambarPK} className="w-full z-0  h-full    object-fill example "></img>
+                </div>
+            </a>
+            <a href="#" className=" shadow-2xl relative ">
+                <div className="  h-full relative shadow-2xl shadow-green-900 overflow-hidden group ">
+                    <div className=" absolute -bottom-10 group-hover:top-0 left-0 w-full h-full group-hover:bg-green-900 transition-all ease-in-out duration-500  ">
+                        <div className="w-full h-full   p-5   relative">
+                            <div className="absolute bottom-0 group-hover:bottom-24 text-white  text-left   transition-all ease-in-out duration-500 ">
+                                <h2 className="mb-2 text-10 font-bold pl-3 pt-1">Pantai Kejawanan</h2>
+                                <p className="mb-2 text-10 font-bold pl-3 pt-1">Lihat Selengkapnya</p>
+                            </div>
+                        </div>
+                    </div>
+                    <img src={GambarPK} className="w-full z-0  h-full    object-fill example "></img>
+                </div>
+            </a>
+            <a href="#" className=" shadow-2xl relative ">
+                <div className="  h-full relative shadow-2xl shadow-green-900 overflow-hidden group ">
+                    <div className=" absolute -bottom-10 group-hover:top-0 left-0 w-full h-full group-hover:bg-green-900 transition-all ease-in-out duration-500  ">
+                        <div className="w-full h-full   p-5   relative">
+                            <div className="absolute bottom-0 group-hover:bottom-24 text-white  text-left   transition-all ease-in-out duration-500 ">
+                                <h2 className="mb-2 text-10 font-bold pl-3 pt-1">Pantai Kejawanan</h2>
+                                <p className="mb-2 text-10 font-bold pl-3 pt-1">Lihat Selengkapnya</p>
+                            </div>
+                        </div>
+                    </div>
+                    <img src={GambarPK} className="w-full z-0  h-full    object-fill example "></img>
+                </div>
+            </a>
+            <a href="#" className=" shadow-2xl relative ">
+                <div className="  h-full relative shadow-2xl shadow-green-900 overflow-hidden group ">
+                    <div className=" absolute -bottom-10 group-hover:top-0 left-0 w-full h-full group-hover:bg-green-900 transition-all ease-in-out duration-500  ">
+                        <div className="w-full h-full   p-5   relative">
+                            <div className="absolute bottom-0 group-hover:bottom-24 text-white  text-left   transition-all ease-in-out duration-500 ">
+                                <h2 className="mb-2 text-10 font-bold pl-3 pt-1">Pantai Kejawanan</h2>
+                                <p className="mb-2 text-10 font-bold pl-3 pt-1">Lihat Selengkapnya</p>
+                            </div>
+                        </div>
+                    </div>
+                    <img src={GambarPK} className="w-full z-0  h-full    object-fill example "></img>
+                </div>
+            </a>
+            <a href="#" className=" shadow-2xl relative ">
+                <div className="  h-full relative shadow-2xl shadow-green-900 overflow-hidden group ">
+                    <div className=" absolute -bottom-10 group-hover:top-0 left-0 w-full h-full group-hover:bg-green-900 transition-all ease-in-out duration-500  ">
+                        <div className="w-full h-full   p-5   relative">
+                            <div className="absolute bottom-0 group-hover:bottom-24 text-white  text-left   transition-all ease-in-out duration-500 ">
+                                <h2 className="mb-2 text-10 font-bold pl-3 pt-1">Pantai Kejawanan</h2>
+                                <p className="mb-2 text-10 font-bold pl-3 pt-1">Lihat Selengkapnya</p>
+                            </div>
+                        </div>
+                    </div>
+                    <img src={GambarPK} className="w-full z-0  h-full    object-fill example "></img>
+                </div>
+            </a>
             <a href="#" className=" shadow-2xl relative ">
                 <div className="  h-full relative shadow-2xl shadow-green-900 overflow-hidden group ">
                     <div className=" absolute -bottom-10 group-hover:top-0 left-0 w-full h-full group-hover:bg-green-900 transition-all ease-in-out duration-500  ">
@@ -122,62 +227,8 @@ function Destinasiwisata() {
                     <img src={GambarPD} className="w-full z-0  h-full    object-fill example "></img>
                 </div>
             </a>
-        </div>
+</div>
         <br/>
-        <div className="grid grid-cols-1 gap-6   lg:grid-cols-4 ">
-            <a href="#" className=" shadow-2xl relative ">
-                <div className="  h-full relative shadow-2xl shadow-green-900 overflow-hidden group ">
-                    <div className=" absolute -bottom-10 group-hover:top-0 left-0 w-full h-full group-hover:bg-green-900 transition-all ease-in-out duration-500  ">
-                        <div className="w-full h-full   p-5   relative">
-                            <div className="absolute bottom-0 group-hover:bottom-24 text-white  text-left   transition-all ease-in-out duration-500 ">
-                                <h2 className="mb-2 text-10 font-bold pl-3 pt-1">Terasering Panyaweuan Argapura</h2>
-                                <p className="mb-2 text-10 font-bold pl-3 pt-1">Lihat Selengkapnya</p>
-                            </div>
-                        </div>
-                    </div>
-                    <img src={GambarTPA} className="w-full z-0  h-full    object-fill example "></img>
-                </div>
-            </a>
-            <a href="#" className=" shadow-2xl relative ">
-                <div className="  h-full relative shadow-2xl shadow-green-900 overflow-hidden group ">
-                    <div className=" absolute -bottom-10 group-hover:top-0 left-0 w-full h-full group-hover:bg-green-900 transition-all ease-in-out duration-500  ">
-                        <div className="w-full h-full   p-5   relative">
-                            <div className="absolute bottom-0 group-hover:bottom-24 text-white  text-left   transition-all ease-in-out duration-500 ">
-                                <h2 className="mb-2 text-10 font-bold pl-3 pt-1">Sunyaragi</h2>
-                                <p className="mb-2 text-10 font-bold pl-3 pt-1">Lihat Selengkapnya</p>
-                            </div>
-                        </div>
-                    </div>
-                    <img src={GambarSunyaragi} className="w-full z-0  h-full    object-fill example "></img>
-                </div>
-            </a>
-            <a href="#" className=" shadow-2xl relative ">
-                <div className="  h-full relative shadow-2xl shadow-green-900 overflow-hidden group ">
-                    <div className=" absolute -bottom-10 group-hover:top-0 left-0 w-full h-full group-hover:bg-green-900 transition-all ease-in-out duration-500  ">
-                        <div className="w-full h-full   p-5   relative">
-                            <div className="absolute bottom-0 group-hover:bottom-24 text-white  text-left   transition-all ease-in-out duration-500 ">
-                                <h2 className="mb-2 text-10 font-bold pl-3 pt-1">Situ Cipatan</h2>
-                                <p className="mb-2 text-10 font-bold pl-3 pt-1">Lihat Selengkapnya</p>
-                            </div>
-                        </div>
-                    </div>
-                    <img src={GambarSC} className="w-full z-0  h-full    object-fill example "></img>
-                </div>
-            </a> 
-            <a href="#" className=" shadow-2xl relative ">
-                <div className="  h-full relative shadow-2xl shadow-green-900 overflow-hidden group ">
-                    <div className=" absolute -bottom-10 group-hover:top-0 left-0 w-full h-full group-hover:bg-green-900 transition-all ease-in-out duration-500  ">
-                        <div className="w-full h-full   p-5   relative">
-                            <div className="absolute bottom-0 group-hover:bottom-24 text-white  text-left   transition-all ease-in-out duration-500 ">
-                                <h2 className="mb-2 text-10 font-bold pl-3 pt-1">Masjid Agung Cirebon</h2>
-                                <p className="mb-2 text-10 font-bold pl-3 pt-1">Lihat Selengkapnya</p>
-                            </div>
-                        </div>
-                    </div>
-                    <img src={GambarMAC} className="w-full z-0  h-full    object-fill example "></img>
-                </div>
-            </a>
-        </div>
         </div>
         <br/>
       <div>
