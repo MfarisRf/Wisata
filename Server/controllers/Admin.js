5b69e0d249fc52fa6e76618217c1f37855ecdff2
MUHAMAD FARIS RAFI <89460895+MfarisRf@users.noreply.github.com>
2023-12-04 03:19:22 +0700

    1

--- Server/controllers/Admin.js
+++ Server/controllers/Admin.js
@@ -35,6 +35,43 @@ export const Register = async (req, res) => {
     }
 }
 
+// export const Login = async (req, res) => {
+//     try {
+//         const admin = await Admin.findAll({
+//         where : { username: req.body.username }
+//     });
+//     const match = await bcrypt.compare(req.body.password, admin[0].password);
+//     if(!match) return res.status(400).json({message: "Password salah"});
+//     const adminId = admin[0].id;
+//     const nama_admin = admin[0].nama_admin;
+//     const username = admin[0].username;
+//     const accessToken = Jwt.sign({adminId, nama_admin, username}, process.env.ACCESS_TOKEN_SECRET,{
+//         expiresIn: "20s"
+//     });
+//     const refreshToken = Jwt.sign({adminId, nama_admin, username}, process.env.REFRESH_TOKEN_SECRET,{
+//         expiresIn: "1d"
+//     });
+//     await Admin.update({refresh_token:refreshToken},{
+//         where:{
+//             id: adminId
+//         }
+//     });
+//     res.cookie('refreshToken', refreshToken, {
+//         httpOnly: true,
+//         maxAge: 24*60*60*1000,
+//     });
+//     res.json({accessToken})
+
+// } catch (error) {
+//     console.log(error);
+//     res.status(404).json({ msg: "Username tidak ditemukan" });
+// }
+
+
+
+
+
+
 export const Login = async (req, res) => {
     try {
         const admin = await Admin.findAll({
