1449b43eb932b347a84c1709c1ba1e8a7e3b0748
MUHAMAD FARIS RAFI <89460895+MfarisRf@users.noreply.github.com>
2023-12-12 14:55:11 +0700

    UPDATE BACKEND BOSS

--- Server/config/dbConfig.js
+++
@@ -1,9 +0,0 @@
-// /config/dbConfig.js
-import { Sequelize } from 'sequelize';
-
-const sequelize = new Sequelize('db_wisata', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
-
-export default sequelize;
---
+++ Server/index.js
@@ -0,0 +1,58 @@
+import express from 'express';
+import cors from 'cors';
+import session from 'express-session';
+import dotenv from 'dotenv';
+import UserRoute from './routes/UserRoute.js';
+import WisataRoute from './routes/WisataRoute.js';
+import AuthRoute from './routes/AuthRoute.js';
+import db from './config/Database.js';
+import SequelizeStore from 'connect-session-sequelize';
+
+dotenv.config();
+
+// (async()=>{
+//     await db.sync();
+// })();
+
+const app = express();
+
+const sessionStore = SequelizeStore(session.Store);
+
+const store = new sessionStore({
+    db: db,
+
+});
+
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+  }));
+
+
+app.use(session({
+    secret: 'dwandalkclkamcl',
+    resave: false,
+    saveUninitialized: true,
+    store: store,
+    cookie: { 
+        secure: 'auto' 
+    }
+}));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+
+app.use(express.json());
+app.use(UserRoute);
+app.use(AuthRoute);
+app.use(WisataRoute);
+// store.sync();
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
+});
