d1041b16e5a46e27bdeae124531c3221f6e8a0d1
Riki Pajri <109595793+Rikpaj@users.noreply.github.com>
2023-12-15 01:59:10 +0700

    update

--- Server/routes/OlehOlehRoutes.js
+++ Server/routes/OlehOlehRoutes.js
@@ -8,27 +8,27 @@ import {
 } from "../controllers/detailOleh.js";
 
 import {
-    getTempatKuliner,
-    getTempatKulinerById,
-    createTempatKuliner,
-    updateTempatKuliner,
-    deleteTempatKuliner
-} from "../controllers/TempatKuliner.js"
+    getTempatOleh,
+    getTempatOlehById,
+    createTempatOleh,
+    updateTempatOleh,
+    deleteTempatOleh
+} from "../controllers/TempatOleh.js"
 import {verifyUser} from "../middleware/AuthUser.js";
 
 const router = express.Router();
 //router untuk detail kuliner
-router.get("/kuliner", getKuliner);
-router.get("/kuliner/:id", getKulinerById);
-router.post("/kuliner", verifyUser, createKuliner);
-router.patch("/kuliner/:id", verifyUser,updateKuliner);
-router.delete("/kuliner/:id", verifyUser, deleteKuliner);
+router.get("/Oleh", getOleh);
+router.get("/Oleh/:id", getOlehById);
+router.post("/Oleh", verifyUser, createOleh);
+router.patch("/Oleh/:id", verifyUser,updateOleh);
+router.delete("/Oleh/:id", verifyUser,deleteOleh);
 
 //router untuk tempat kuliner
-router.get("/tempatKuliner", getTempatKuliner);
-router.get("/tempatKuliner/:id", getTempatKulinerById);
-router.post("/tempatKuliner", verifyUser, createTempatKuliner);
-router.patch("/tempatKuliner/:id", verifyUser,updateTempatKuliner);
-router.delete("/tempatKuliner/:id", verifyUser, deleteTempatKuliner);
+router.get("/tempatOleh", getTempatOleh);
+router.get("/tempatOleh/:id", getTempatOlehById);
+router.post("/tempatOleh", verifyUser, createTempatOleh);
+router.patch("/tempatOleh/:id", verifyUser,updateTempatOleh);
+router.delete("/tempatOleh/:id", verifyUser, deleteTempatOleh);
 
 export default router;
