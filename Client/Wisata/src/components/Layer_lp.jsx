4e139928a99ccb0e2b24873112f5f7568c08cb5f
MUHAMAD FARIS RAFI <89460895+MfarisRf@users.noreply.github.com>
2023-12-15 02:53:19 +0700

    1\

--- Client/Wisata/src/components/Layer_lp.jsx
+++ Client/Wisata/src/components/Layer_lp.jsx
@@ -1,6 +1,7 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
-import background from "../assets/images/Teksturs3.png";
+import React from "react";
+
+import background from "../assets/images/teksturs3.png";
 
 const Layer_lp = () => {
   return (
