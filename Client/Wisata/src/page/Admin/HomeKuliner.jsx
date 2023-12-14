bd8363eb8a2cc46864264f7730314e8207327c36
arya surya pratama <145951543+AryaSuryaPratama@users.noreply.github.com>
2023-12-15 01:52:02 +0700

    kuliner

--- Client/Wisata/src/page/Admin/HomeKuliner.jsx
+++ Client/Wisata/src/page/Admin/HomeKuliner.jsx
@@ -1,10 +1,29 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React, { useState, useEffect } from "react";
+// import { Link } from "react-router-dom";
+import axios from "axios";
+// import React from 'react'
 import Navbar_admin from '../../components/Navbar_admin '
 import Gambar from '../../assets/images/Ghandi.jpg'
 import FooterAdmin from '../../components/FooterAdmin'
 
 const HomeWisata = () => {
+   const [Kuliner, setKuliner] = useState([]);
+
+
+   useEffect(()=>{
+      getKuliner();
+   },[]);
+
+   const getKuliner = async () =>{
+      const response = await axios.get('http://localhost:5000/Kuliner');
+      setKuliner(response.data);
+   };
+
+   const deleteKuliner = async (KulinerId) =>{
+      await axios.delete(`http://localhost:5000/Kuliner/${KulinerId}`);
+      getKuliner();
+   }
   return (
     <div>
    <Navbar_admin/>
@@ -128,33 +147,45 @@ const HomeWisata = () => {
             </tr>
         </thead>
         <tbody>
-            <tr className="bg-[#F1F1E8] border-b dark:bg-gray-800 border-2 dark:border-[#206A5D] hover:bg-[#BFDCAE] dark:hover:bg-gray-600 text-center text-[#6FA385]">
+         {Kuliner.map((Kuliner, index) => (
+            <tr key={Kuliner.uuid} className="bg-[#F1F1E8] border-b dark:bg-gray-800 border-2 dark:border-[#206A5D] hover:bg-[#BFDCAE] dark:hover:bg-gray-600 text-center text-[#6FA385]">
                 <th scope="row" className="px-6 py-4 font-medium text-[#6FA385] whitespace-nowrap dark:text-white">
                 Empal Gentong
                 </th>
                 <td className="px-6 py-4">
+                 {index + 1} 
                 07.00
                 </td>
                 <td className="px-6 py-4">
+                 {Kuliner.name}
                 21.00 
                 </td>
                 <td className="px-6 py-4">
+                 {Kuliner.price} 
                 Empal Gentong Bu Darma
                 </td>
                 <td className="px-6 py-4">
+                 {Kuliner.user.name} 
                 Cirebon
                 </td>
+
                 <td className="px-6 py-4 text-right">
+                  <link to ={`/Kuliner/edit/${Kuliner.uuid}`} className="button is-small is-info">
                     <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline"><svg className="w-6 h-6 text-[#0B56C8] dark:text-white ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 21 21">
                      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7.418 17.861 1 20l2.139-6.418m4.279 4.279 10.7-10.7a3.027 3.027 0 0 0-2.14-5.165c-.802 0-1.571.319-2.139.886l-10.7 10.7m4.279 4.279-4.279-4.279m2.139 2.14 7.844-7.844m-1.426-2.853 4.279 4.279"/>
                      </svg></a>
+                  </link>
+                  
                 </td>
                 <td className="px-6 py-4 text-right">
+                <button onClick={() => deleteKuliner(Kuliner.uuid)} className="button is-small is-danger">
+                </button>
                     <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline"><svg className="w-6 h-6 text-[#FF0606] dark:text-white ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
                      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h16M7 8v8m4-8v8M7 1h4a1 1 0 0 1 1 1v3H6V2a1 1 0 0 1 1-1ZM3 5h12v13a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V5Z"/>
                      </svg></a>
                 </td>
             </tr>
+         ))}
             <tr className="bg-[#F1F1E8] border-b dark:bg-gray-800 border-2 dark:border-[#206A5D] hover:bg-[#BFDCAE] dark:hover:bg-gray-600">
                 <th scope="row" className="px-6 py-4 font-medium text-[#6FA385] whitespace-nowrap dark:text-white text-center">
                 Lotek
