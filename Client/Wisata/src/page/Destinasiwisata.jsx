62fd726bd94697dfea89628496869870e16c25c5
MUHAMAD FARIS RAFI <89460895+MfarisRf@users.noreply.github.com>
2023-12-19 19:37:46 +0700

    1

--- Client/Wisata/src/page/Destinasiwisata.jsx
+++ Client/Wisata/src/page/Destinasiwisata.jsx
@@ -7,6 +7,7 @@ import Footer from '../components/Footer'
 import 'flowbite'
 import background from "../assets/images/Backgroud_kwo.png"
 import Card_wisata from '../components/Card_wisata'
+import { Link } from 'react-router-dom'
 
 // import background from "../assets/images/Teksturs.png";
 import GambarExploreWisata from '../assets/images/ExplorWisata.png'
@@ -15,6 +16,7 @@ import GambarExploreWisata from '../assets/images/ExplorWisata.png'
 function Destinasiwisata() {
   const [category, setCategory] = useState([]);
    const [Wisata, setWisata] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     getWisata();
@@ -34,9 +36,14 @@ const getWisata = async () => {
      const response = await axios.get('http://localhost:5000/Wisata');
      setWisata(response.data);
 }
+const handleSearchChange = (event) => {
+  setSearchTerm(event.target.value);
+};
 
-
-
+const filteredWisata = Wisata.filter((record) =>
+    
+    record.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
   return (
     <div className='bg-cover' style={{backgroundImage: `url(${background}) `}}>
     <Navbar/>
@@ -49,7 +56,11 @@ const getWisata = async () => {
 
   <div className="flex items-center justify-between w-full sm:w-96">
   <div className="relative w-full mr-2 pl-20">
-  <input type="search" id="search-dropdown" className="block p-2.5 w-72 z-20 text-sm text-gray-900 rounded-lg border-s-[#2D8C14] border-s-1 border border-[#2D8C14] focus:ring-[#2D8C14] focus:border-[#2D8C14] dark:bg-gray-700 dark:border-s-gray-700 dark:border-[#2D8C14] dark:placeholder-gray-400 dark:text-white" placeholder="Search" required />
+  <input type="search" 
+  id="search-dropdown" 
+  value={searchTerm}
+  onChange={handleSearchChange}
+  className="block p-2.5 w-72 z-20 text-sm text-gray-900 rounded-lg border-s-[#2D8C14] border-s-1 border border-[#2D8C14] focus:ring-[#2D8C14] focus:border-[#2D8C14] dark:bg-gray-700 dark:border-s-gray-700 dark:border-[#2D8C14] dark:placeholder-gray-400 dark:text-white" placeholder="Search" required />
 
   <button type="submit" className="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-gray-900 bg-[#D5EDC7] bg-opacity-80 rounded-lg border border-[#2D8C14] hover:bg-opacity-90 focus:ring-1 focus:outline-none focus:ring-[#2D8C14] dark:bg-gray-800 dark:border-s-gray-700 dark:border-gray-700 dark:text-white dark:hover:bg-gray-900">
     <svg className="w-10 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
@@ -89,9 +100,29 @@ const getWisata = async () => {
 <br/>
 {/* ini awal card */}
 <div>
-  <Card_wisata key={Wisata.uuid} data={Wisata} />
-
-    </div>
+  <div className="grid px-12 grid-cols-1 gap-6 lg:grid-cols-4 ">
+  {filteredWisata.map((record) => (
+            <div href="" className=" shadow-2xl relative ">
+                <div className="  h-full relative shadow-md rounded-lg shadow-black overflow-hidden group ">
+                    <div className=" absolute -bottom-10 group-hover:top-0 left-0 w-full h-full group-hover:bg-[#2D8C14] transition-all ease-in-out duration-500  ">
+                        <div className="w-full h-full p-5 relative">
+                            <div className="absolute bottom-0 group-hover:bottom-24 text-white  text-left   transition-all ease-in-out duration-500 ">
+                                <h2 className="mb-2 text-10 font-bold pl-3 pt-1 text-[#F1F1E8] ">{record.name}</h2>
+                                <Link to={`/destinasiwisata/detailwisata/${record.uuid}`} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-[#F1F1E8]">
+                                lihat selengkapnya
+                                <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                                </svg>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                    <img src={record.image} className="w-full z-0  h-full    object-fill example "></img>
+                </div>
+            </div>
+            ))}
+            </div>
+        <br/>
+        </div>
 {/* ini akhir card */}
         <br/>
       <div>
